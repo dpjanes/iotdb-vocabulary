260d65c20ba87699a892835cf9f9fa27df5ee769
David Janes <davidjanes@davidjanes.com>
2015-03-10 10:05:35 -0400

    documentation

---
+++ DoCompose.py
@@ -0,0 +1,158 @@
+#
+#   DoCompose.py
+#
+#   David Janes
+#   IOTDB
+#   2015-03-07
+#
+#   Take all the files in "src/" and
+#   compose the JSON-LD files.
+#
+#   See README.md for more details
+#
+
+import sys
+import os
+import json
+import re
+
+SRC_FOLDER = "src/"
+DST_FOLDER = "."
+TOP_NAMES = [
+    "iot",
+    "iot-attribute",
+    "iot-facet",
+    "iot-unit",
+]
+
+SCHEMA_NAMES = [ 
+    "name", 
+    "description", 
+    "url", 
+    "image", 
+    "sameAs", 
+]
+
+NUMERIC_NAMES = [
+    "iot:minimum",
+    "iot:maximum",
+]
+
+def cook_type(type_name):
+    if re.match("^[A-Z]", type_name):
+        return "iot:" + type_name
+    elif type_name == "id":
+        return "@id"
+    elif type_name == "class":
+        return "rdfs:Class"
+    else:
+        return "xs:" + type_name
+
+def file_loader(fin):
+    d = {}
+    for line in fin:
+        line = line.strip()
+        if not line or line.startswith('#'):
+            continue
+
+        parts = line.split(' ', 1)
+        if len(parts) != 2:
+            continue
+
+        tag, value = parts
+        tag = tag.strip()
+        value = value.strip()
+
+        if tag.find(':') == -1:
+            ## http://schema.org/Thing
+            if tag in SCHEMA_NAMES:
+                tag = "schema:" + tag
+            else:
+                tag = "iot:" + tag
+
+        if tag in NUMERIC_NAMES:
+            if value.find('.') == -1:
+                value = int(value)
+            else:
+                value = float(value)
+
+        d.setdefault(tag, []).append(value)
+
+    for key, value in list(d.items()):
+        if len(value) == 1:
+            d[key] = value[0]
+
+    return d
+
+def top_loader(top_name):
+    ds = []
+    top_folder = os.path.join(SRC_FOLDER, top_name)
+
+    for type_name in os.listdir(top_folder):
+        if type_name.startswith("."):
+            continue
+
+        type_folder = os.path.join(top_folder, type_name)
+
+        type_type = cook_type(type_name)
+
+        for file_name in os.listdir(type_folder):
+            if file_name.startswith("."):
+                continue
+
+            file_path = os.path.join(type_folder, file_name)
+            filed = file_loader(open(file_path))
+            filed["@type"] = type_type
+            filed["@id"] = "https://iotdb.org/pub/%s#%s" % ( top_name, file_name )
+            filed.setdefault("schema:name", re.sub("^.*[.]", "", file_name))
+
+            if type_type == "rdfs:Class":
+                filed["rdfs:isDefinedBy"] = 'iot:' + file_name
+
+            ds.append(filed)
+
+    return ds
+
+d_types = set()
+for top_name in TOP_NAMES:
+    contextd = {
+        "schema": "https://schema.org/",
+        "iot": "https://iotdb.org/pub/iot#",
+        "iot-unit": "https://iotdb.org/pub/iot-unit#",
+        "iot-attribute": "https://iotdb.org/pub/iot-attribute#",
+        "iot-facet": "https://iotdb.org/pub/iot-facet#",
+        "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+        "rdfs" : "http://www.w3.org/2000/01/rdf-schema#",
+        "xs": "http://www.w3.org/2001/XMLSchema/#",
+    }
+    ds = top_loader(top_name)
+    jsonld = {
+        "@id": "https://iotdb.org/pub/%s" % top_name,
+        "@context": contextd,
+        "iot:associated" : ds
+    }
+
+    ## identify types
+    for d in ds:
+        if d["@type"] != "@id":
+            continue
+
+        d_id = d["@id"]
+        d_id = re.sub("^.*/", "", d_id)
+        d_id = re.sub("#", ":", d_id)
+        d_types.add(d_id)
+
+    ## add to context (this makes NS URL values expand in JSON-LD)
+    for d in ds:
+        for key in d.iterkeys():
+            if key not in d_types:
+                continue
+
+            contextd[key] = {
+                "@type": "@id"
+            }
+
+
+    top_dst = os.path.join(DST_FOLDER, "%s.jsonld" % top_name)
+    with open(top_dst, 'w') as tout:
+        print >> tout, json.dumps(jsonld, sort_keys=True, indent=2)
--- README.md
+++ README.md
@@ -14,19 +14,124 @@ The most likely files you may wish to change are
 * iot-facet.jsonld
 * iot-placement.jsonld
 
-## Copyright
+## Schema.org
 
-    Copyright (C) 2014 David P Janes
+We've moved from using iot: names to schema.org names
+from [Thing](http://schema.org/Thing). Specifically
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+* additionalType  
+* alternateName  
+* name    
+* description 
+* image   
+* sameAs  
+* url
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+## Structure
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+The source files are in "src". The hierarchical structure of 
+the folders is:
+
+* iot-unit|iot-facet|iot-placement|iot
+* @type
+* @id (in particular, the part after #) - a file
+
+The files are a simple text format, a
+key followed by a space followed by a value.
+Keys may repeat multiple times
+
+e.g.
+
+    description when to end / finish something
+    format iot:date
+    format iot:time
+    format iot:datetime
+
+the <code>schema:name</code>, <code>@type</code> and <code>@id</code>
+are automatically made by the DoCompose function.
+
+The program <code>DoCompose.py</code> converts
+the files in <code>src</code> to JSON-LD in this 
+directory.
+
+## Making changes
+
+* Fork this project
+* Add whatever to src/iot-[something]/[@type]/[name]  (See Units of Measure below for rules specific to that)
+* Commit
+* Push
+
+If accepted, they will be officially published
+at [https://iotdb.org/pub](https://iotdb.org/pub)
+
+## Units of Measure
+
+The Units of Measure are in <code>iot-unit.jsonld</code>
+from the directory <code>src/iot-unit/Unit</code>
+
+Each unit of measure filename must have three parts, separated
+by '.':
+
+* the _property_
+* the _system_
+* the _name_
+
+The property is very loosey-goosey and is meant for _humans_
+to read and understand and is based on colloquial understandings
+of a word rather than a formal defintion. For example, speed and
+acceleration are grouped together under "speed"; 
+candela, lux and lumen are grouped together under "light".
+
+The property **must** be one of the following:
+
+* area
+* energy
+* length
+* light
+* mass
+* math
+* pressure
+* radioactivity
+* speed
+* temperature
+* time
+* volume
+
+The _system_ **must** be one of the following
+
+* angle
+* constant
+* fraction
+* imperial
+* si
+* traditional
+* troy
+* us
+
+The name is exactly what you'd expect.
+
+### Adding New Units of Measure
+
+Important:
+
+* following the three part convention
+* do not add new things unless you're planning to use it
+* try not add SI multiplier units (e.g. centigram) unless
+they're very commonly used. Exponents can be added
+to values if this is needed.
+* add conversions
+
+### Conversions
+
+Conversions are rules about how to convert _other_ units
+to this one. For example, <code>temperature.si.celsius</code>
+has the following conversions in it:
+
+    conversion [temperature.si.kelvin]-272.15
+    conversion ([temperature.imperial.fahrenheit]-32)/5*9
+
+The format should be fairly obvious.
+
+Add conversions to at least one other unit in the system
+if at all possible. Assume the system will know how to
+chain conversions, so don't get carried away.
